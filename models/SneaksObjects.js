9ddcd82f995e64b23a16c5a4c9877e60edfdc688
Evan Grillo <evansmacbook@dhcp-10-105-220-115.wireless.northwestern.private>
2018-03-26 18:49:15 -0500

    updating

--- models/SneaksObjects.js
+++ models/SneaksObjects.js
@@ -46,7 +46,7 @@ module.exports = function(sequelize, DataTypes) {
 	    	defaultValue: false
 	    }
 
-	    };
+	    });
 
 Shoes.associate = function(models) {
     // We're saying that a Post should belong to an Author
@@ -63,34 +63,3 @@ Shoes.associate = function(models) {
 
 };
 
-module.exports = function(sequelize, DataTypes) {
-  var Sellers = sequelize.define("Sellers", {
-  		id: {
-		    type: DataTypes.INTEGER,
-		    autoIncrement: true,
-		    primaryKey: true,
-		},
-		name: {
-			type: DataTypes.STRING,
-			allowNull: false
-		},
-		posts: {
-			type: DataTypes.INTEGER,
-			allowNull: true
-		},
-		sales: {
-			type: DataTypes.INTEGER,
-			allowNull: true
-		}
-
-Sellers.associate = function(models) {
-    // Associating Author with Posts
-    // When an Author is deleted, also delete any associated Posts
-    Sellers.hasMany(models.Shoes, {
-      onDelete: "cascade"
-    });
- };
-
-  return Sellers;
-
-};
