fa17860d2b386ace356ef3d0b0de3f7284669e32
Chris Hutchinson <chrisbond19@me.com>
2018-03-30 23:02:28 -0500

    Fixed modals, bidding, and buy now button

--- public/js/bid.js
+++ public/js/bid.js
@@ -1,43 +1,55 @@
 $(document).ready(function () {
-	$("#modal-button").on("click", function(event){
-	    event.preventDefault();
+ 
+		$('#thebid').on('show.bs.modal', function (event) {
+			
+		  var button = $(event.relatedTarget); // Button that triggered the modal
+		  var name = button.data('name');
+		  var currentbid = button.data('highbid');
+		  var brand = button.data('brand');
+		  var id = button.data('id');
+		  var minimumbid = currentbid + 1;
+		  var shoeBid = $("#minimumbid").val().trim();
+		   
+		  var modal = $(this);
+		  modal.find('#shoe-modal-title').text(brand + " " + name);
+		  modal.find('#modal-price').text("$" + currentbid);
+		  modal.find('#lowbid').text("Lowest Bid: " + minimumbid);
+		  $("#bid-button").on("click", function(event){
+        event.preventDefault();
 
-	    var objectId = $("#sneakId").html();
-	    console.log(objectId);
-
-	    var currentBid = $("#current-bid").text();
-
-	    console.log(currentBid);
-
-	    alert("placing bid");
-	    var shoeBid = $("#minimumbid").val().trim();
-	    var newBid = parseInt(shoeBid);
-
-	    console.log(newBid);
-
-	    if (newBid > currentBid) {
-
-	    	alert("New Highest Bid = $" + newBid);
-
+		  if (shoeBid > currentbid) {
+		  	updateBid();
+		  
+		  }
+		  else{
+		  	alert("The current bid is $" + currentbid);
+		  }
+});
 		    function updateBid(Shoes) {
 				    $.ajax({
-				      method: "PUT",
+				      method: "UPDATE",
 				      url: "/api/newObject",
 				      data: { 
-				      	highest_bid: JSON.stringify(newBid),
+				      	highest_bid: JSON.stringify(shoeBid),
 				      },
 				      	where: {
-				      		id: parseInt(objectId),
+				      		id: id,
 				      	}
 				    }).then(Shoes);
 				  }
-			//call function
-			updateBid();
-		}
-		else {
-			alert("The current highest bid is $" + currentBid);
-		}
 
-	  });
+	});
+		$('#thebuy').on('show.bs.modal', function (event) {
+			
+		  var button = $(event.relatedTarget); // Button that triggered the modal
+		  var buynow = button.data('buy');
+		 var namebuy = button.data('name');
+		 var brandbuy = button.data('brand');   
+		  var modal = $(this);
+
+		  modal.find('#shoe-modal-name').text(brandbuy + " " + namebuy);
+		  modal.find('#modal-buynow').text("$" + buynow);
+		 
+		});
 });
 
--- public/js/sell.js
+++ public/js/sell.js
@@ -21,6 +21,7 @@ $(document).ready(function () {
       .then(function(data) {
       console.log(data);
     });
-  });
+  })
+
 });
 
